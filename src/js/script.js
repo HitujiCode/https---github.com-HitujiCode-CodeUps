810fddf1209ece913b40f5235c5728c94aaf1286
hituji <k.m.hituji3340@gmail.com>
2023-07-17 23:48:28 +0900

    campaign-cardまで

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,34 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+    let scrollTop = 0;
 
+    $(".js-hamburger, .js-sp-nav").click(function () {
+      $(".js-hamburger,.js-sp-nav").toggleClass("is-active");
+      if($("body").hasClass("is-fixed")){
+        // スクロール開放
+        $("body").css("top","");
+        $("body").removeClass("is-fixed");
+        window.scrollTo(0, scrollTop);
+      } else {
+        // スクロール禁止
+        scrollTop = $(window).scrollTop();
+        $("body").css("top", -scrollTop + "px");
+        $("body").addClass("is-fixed");
+      }
+    //   swiper.update();
+    });
+    
+    $(window).resize(function() {
+      if ($(window).width() >= 768) {
+        if ($(".js-hamburger").hasClass("is-active")) {
+          // スクロール開放
+          $("body").css("top","");
+          $("body").removeClass("is-fixed");
+          window.scrollTo(0, scrollTop);
+          $(".js-hamburger").removeClass("is-active");
+          $(".js-sp-nav").fadeOut();
+        }
+      }
+    });
 });
